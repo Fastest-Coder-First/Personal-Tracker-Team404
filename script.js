12f11b878e9b5f3e451ea22d7c581ebdb33ce389
GK-Idiot <gorantlakartikeya@gmail.com>
2023-06-24 16:01:47 +0530

    css changes

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const addButton = document.getElementById('add-button');
   addButton.addEventListener('click', addExpense);
 
   const expenseContainer = document.getElementById('expense-container');
-  expenseContainer.addEventListener('input', function(event) {
+  expenseContainer.addEventListener('input', function (event) {
     if (event.target.classList.contains('price-input')) {
       updateTotalCost();
     }
   });
 
-  expenseContainer.addEventListener('click', function(event) {
+  expenseContainer.addEventListener('click', function (event) {
     if (event.target.classList.contains('delete-button')) {
       deleteExpense(event.target.parentElement);
     }
@@ -41,9 +41,9 @@ function addExpense() {
   priceInput.addEventListener('input', updateTotalCost);
 
   const deleteButton = document.createElement('button');
-  deleteButton.textContent = '-';
+  deleteButton.textContent = '🗑️';
   deleteButton.classList.add('delete-button');
-  deleteButton.addEventListener('click', function() {
+  deleteButton.addEventListener('click', function () {
     deleteExpense(expenseLabel);
   });
 
@@ -67,7 +67,7 @@ function updateTotalCost() {
   const priceInputs = document.querySelectorAll('.price-input');
   let totalCost = 0;
 
-  priceInputs.forEach(function(input) {
+  priceInputs.forEach(function (input) {
     const price = parseFloat(input.value);
     if (!isNaN(price)) {
       totalCost += price;
