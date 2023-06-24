03e960cc817544fa8c8506b39e17e606c58b1ef8
Vishnu Gamini <gamini.vishnu@gmail.com>
2023-06-24 14:31:57 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const addButton = document.getElementById('add-button');
+    addButton.addEventListener('click', addExpense);
+  
+    const expenseContainer = document.getElementById('expense-container');
+    expenseContainer.addEventListener('input', function(event) {
+      if (event.target.classList.contains('price-input')) {
+        updateTotalCost();
+      }
+    });
+  
+    expenseContainer.addEventListener('click', function(event) {
+      if (event.target.classList.contains('delete-button')) {
+        deleteExpense(event.target);
+      }
+    });
+  
+    updateTotalCost(); // Update total cost initially
+  });
+  
+  function addExpense() {
+    const expenseContainer = document.getElementById('expense-container');
+  
+    const expenseLabel = document.createElement('div');
+    expenseLabel.classList.add('expense-label');
+  
+    const labelInput = document.createElement('input');
+    labelInput.type = 'text';
+    labelInput.placeholder = 'Label';
+  
+    const priceInput = document.createElement('input');
+    priceInput.type = 'number';
+    priceInput.placeholder = 'Price';
+    priceInput.classList.add('price-input');
+    priceInput.addEventListener('input', updateTotalCost);
+  
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '-';
+    deleteButton.classList.add('delete-button');
+    deleteButton.addEventListener('click', function() {
+      deleteExpense(expenseLabel);
+    });
+  
+    expenseLabel.appendChild(labelInput);
+    expenseLabel.appendChild(priceInput);
+    expenseLabel.appendChild(deleteButton);
+  
+    expenseContainer.appendChild(expenseLabel);
+  
+    updateTotalCost();
+  }
+  
+  function deleteExpense(expenseLabel) {
+    const expenseContainer = document.getElementById('expense-container');
+    expenseContainer.removeChild(expenseLabel);
+  
+    updateTotalCost();
+  }
+  
+  function updateTotalCost() {
+    const priceInputs = document.querySelectorAll('.price-input');
+    let totalCost = 0;
+  
+    priceInputs.forEach(function(input) {
+      const price = parseFloat(input.value);
+      if (!isNaN(price)) {
+        totalCost += price;
+      }
+    });
+  
+    const totalCostElement = document.getElementById('total-cost');
+    totalCostElement.textContent = totalCost.toFixed(2);
+  }
+  
