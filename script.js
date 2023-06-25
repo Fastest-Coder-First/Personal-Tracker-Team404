151703d4e0e23a07274581257cd422ea919bec2d
Vishnu Gamini <gamini.vishnu@gmail.com>
2023-06-25 10:05:00 +0530

    changes made

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
   const addButton = document.getElementById('add-button');
   addButton.addEventListener('click', addExpense);
 
+  const summaryButton = document.getElementById('summary-button');
+  summaryButton.addEventListener('click', showSummary);
+
   const expenseContainer = document.getElementById('expense-container');
   expenseContainer.addEventListener('input', function(event) {
     if (event.target.classList.contains('price-input')) {
@@ -14,6 +17,9 @@ document.addEventListener('DOMContentLoaded', function() {
       deleteExpense(event.target.parentElement);
     }
   });
+
+  // Load previously added expenses from localStorage
+  loadExpenses();
 
   updateTotalCost(); // Update total cost initially
 
@@ -79,4 +85,67 @@ function updateTotalCost() {
   totalCostElement.textContent = totalCost.toFixed(2);
 }
 
+function showSummary() {
+  const expenseLabels = document.querySelectorAll('.expense-label');
+  const summaryData = [];
 
+  expenseLabels.forEach(function(label) {
+    const labelInput = label.querySelector('.label-input');
+    const priceInput = label.querySelector('.price-input');
+
+    const expense = {
+      label: labelInput.value,
+      price: parseFloat(priceInput.value),
+    };
+
+    summaryData.push(expense);
+  });
+
+  localStorage.setItem('summaryData', JSON.stringify(summaryData));
+
+  window.location.href = 'summary.html';
+}
+
+function loadExpenses() {
+  const savedExpenses = localStorage.getItem('summaryData');
+  if (savedExpenses) {
+    const expenses = JSON.parse(savedExpenses);
+
+    expenses.forEach(function(expense) {
+      const expenseLabel = createExpenseLabel(expense.label, expense.price);
+      const expenseContainer = document.getElementById('expense-container');
+      expenseContainer.appendChild(expenseLabel);
+    });
+  }
+}
+
+function createExpenseLabel(labelValue, priceValue) {
+  const expenseLabel = document.createElement('div');
+  expenseLabel.classList.add('expense-label');
+
+  const labelInput = document.createElement('input');
+  labelInput.type = 'text';
+  labelInput.placeholder = 'Label';
+  labelInput.classList.add('label-input');
+  labelInput.value = labelValue;
+
+  const priceInput = document.createElement('input');
+  priceInput.type = 'number';
+  priceInput.placeholder = 'Price';
+  priceInput.classList.add('price-input');
+  priceInput.addEventListener('input', updateTotalCost);
+  priceInput.value = priceValue;
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = ' 🗑️';
+  deleteButton.classList.add('delete-button');
+  deleteButton.addEventListener('click', function() {
+    deleteExpense(expenseLabel);
+  });
+
+  expenseLabel.appendChild(labelInput);
+  expenseLabel.appendChild(priceInput);
+  expenseLabel.appendChild(deleteButton);
+
+  return expenseLabel;
+}
---
+++ summary.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const summaryContainer = document.getElementById('summary-container');
+    const summaryData = JSON.parse(localStorage.getItem('summaryData'));
+  
+    if (summaryData) {
+      summaryData.forEach(function(expense, index) {
+        const itemDiv = createSummaryItem(index + 1, expense.label, expense.price);
+        summaryContainer.appendChild(itemDiv);
+      });
+  
+      const totalCost = summaryData.reduce(function(acc, expense) {
+        return acc + expense.price;
+      }, 0);
+  
+      const totalCostElement = document.getElementById('summary-total-cost');
+      totalCostElement.textContent = totalCost.toFixed(2);
+    }
+  
+    const backButton = document.getElementById('back-button');
+    backButton.addEventListener('click', function() {
+      window.location.href = 'index.html'; // Go back to the home page
+    });
+  });
+  
+  function createSummaryItem(index, label, price) {
+    const itemDiv = document.createElement('div');
+    itemDiv.classList.add('summary-item');
+  
+    const indexSpan = document.createElement('span');
+    indexSpan.textContent = index + '.) ';
+    indexSpan.classList.add('summary-index');
+  
+    const labelSpan = document.createElement('span');
+    labelSpan.classList.add('summary-label');
+    labelSpan.textContent = label;
+  
+    const priceSpan = document.createElement('span');
+    priceSpan.textContent = price.toFixed(2);
+  
+    itemDiv.appendChild(indexSpan);
+    itemDiv.appendChild(labelSpan);
+    itemDiv.appendChild(priceSpan);
+  
+    return itemDiv;
+  }
+  
